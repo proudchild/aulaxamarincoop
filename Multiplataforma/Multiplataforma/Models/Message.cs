d3a6022bda770a0695eb0c378dbe7cdcac9524dc
Sergio Migueis <sergio.migueis.tigre@gmail.com>
2021-08-26 20:00:48 -0300

    O GITHUB TA DE SACANAGEM COMIGO

---
+++ Multiplataforma/Multiplataforma/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multiplataforma.Models
+{
+    public class Message
+    {
+        public String message { get; set; }
+    }
+}
